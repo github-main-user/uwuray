5ae22dd8f2627a6cc80015520841a419d8bcab53
github-main-user <kolya.skripn@mail.ru>
2026-01-07 17:34:40 +0300

    feat: implement config

---
+++ src/config.py
@@ -0,0 +1,21 @@
+import os
+from pathlib import Path
+
+
+class Settings:
+    _CONFIG_DIR = Path(os.environ.get("XDG_CONFIG_HOME", Path.home() / ".config"))
+
+    def __init__(self) -> None:
+        self.APP_CONFIG_PATH: Path = self._CONFIG_DIR / "uwuray"
+        self.APP_CONFIG_PATH.mkdir(exist_ok=True)
+
+        self.SUBSCRIPTION: Path = self.APP_CONFIG_PATH / "subscription.txt"
+        self.SUBSCRIPTION.touch()
+
+        self.PRESETS: Path = self.APP_CONFIG_PATH / "presets.json"
+        self.PRESETS.touch()
+
+        self.CONFIG_TEMPLATE: Path = self.APP_CONFIG_PATH / "config_template.json"
+
+
+settings = Settings()
