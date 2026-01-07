ffcfb840dbd33a97453f722c8996e710393aaed2
github-main-user <kolya.skripn@mail.ru>
2026-01-07 19:38:56 +0300

    fix: create template config file if there is no such file

--- src/config.py
+++ src/config.py
@@ -4,8 +4,11 @@ from pathlib import Path
 
 class Settings:
     _CONFIG_DIR = Path(os.environ.get("XDG_CONFIG_HOME", Path.home() / ".config"))
+    _BASE_DIR = Path(__file__).parent.parent
 
     def __init__(self) -> None:
+        print(self._BASE_DIR)
+
         self.APP_CONFIG_PATH: Path = self._CONFIG_DIR / "uwuray"
         self.APP_CONFIG_PATH.mkdir(exist_ok=True)
 
@@ -16,6 +19,10 @@ class Settings:
         self.PRESETS.touch()
 
         self.CONFIG_TEMPLATE: Path = self.APP_CONFIG_PATH / "config_template.json"
+        if not self.CONFIG_TEMPLATE.exists():
+            self.CONFIG_TEMPLATE.write_text(
+                (self._BASE_DIR / "src/config_template.json").read_text()
+            )
 
         self.PREVIOUS_PRESET: Path = self.APP_CONFIG_PATH / "previous_preset.txt"
         self.PREVIOUS_PRESET.touch()
