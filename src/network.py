5c9203f91b4dc1e27c826f9e56940a14ce109988
github-main-user <kolya.skripn@mail.ru>
2026-02-15 13:35:35 +0300

    fix: spoof headers to bypass app is not supported issue

--- src/network.py
+++ src/network.py
@@ -22,6 +22,12 @@ def is_subscription_url_reachable(url: str) -> bool:
 
 
 def fetch_subscription_data(url: str) -> list[str]:
-    response = requests.get(url)
+    response = requests.get(
+        url,
+        headers={
+            "User-Agent": "v2RayTun/v5.20.65 Platform/linux",
+            "x-hwid": "00000000",
+        },
+    )
     response.raise_for_status()
     return base64.b64decode(response.text).decode().splitlines()
